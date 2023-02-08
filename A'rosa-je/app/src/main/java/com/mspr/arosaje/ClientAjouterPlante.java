07cde4466a2225fb6d23f479a572634d3af4b907
Erwan8989 <77726753+Erwan8989@users.noreply.github.com>
2023-02-08 12:17:04 +0100

    Première version fonctionnelle

--- A'rosa-je/app/src/main/java/com/mspr/arosaje/ClientAjouterPlante.java
+++ A'rosa-je/app/src/main/java/com/mspr/arosaje/ClientAjouterPlante.java
@@ -1,18 +1,24 @@
 package com.mspr.arosaje;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ClientAjouterPlante extends AppCompatActivity {
 
     Button b1;
+    ImageView click_image_id;
+
+    private static final int pic_id = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +27,51 @@ public class ClientAjouterPlante extends AppCompatActivity {
 
         b1 = (Button) findViewById(R.id.btn_resgisterPlant);
 
+        // By ID we can get each component
+        // which id is assigned in XML file
+        // get Buttons and imageview.
+        click_image_id = (ImageView) findViewById(R.id.click_image);
 
+        // Camera_open button is for open the camera
+        // and add the setOnClickListener in this button
         b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Create the camera_intent ACTION_IMAGE_CAPTURE
+                // it will open the camera for capture the image
+                Intent camera_intent
+                        = new Intent(MediaStore
+                        .ACTION_IMAGE_CAPTURE);
+
+                // Start the activity with camera_intent,
+                // and request pic id
+                startActivityForResult(camera_intent, pic_id);
+            }
+        });
+
+        /*b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v2) {
                 Ajout_plante();
             }
-        });
+        });*/
+    }
+
+    // This method will help to retrieve the image
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        // Match the request 'pic id with requestCode
+        if (requestCode == pic_id) {
+
+            // BitMap is data structure of image file
+            // which stor the image in memory
+            Bitmap photo = (Bitmap) data.getExtras()
+                    .get("data");
+
+            // Set the image in imageview for display
+            click_image_id.setImageBitmap(photo);
+        }
     }
 
     @Override
