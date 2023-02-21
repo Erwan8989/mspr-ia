96d700e9d603dd71fd82677f6a30c2ca06cae1db
Erwan8989 <77726753+Erwan8989@users.noreply.github.com>
2023-02-21 06:16:38 +0100

    Section commentaire accueil client (ok)

--- A'rosa-je/app/src/main/java/com/mspr/arosaje/client/ClientChoixArticleAccueil.java
+++ A'rosa-je/app/src/main/java/com/mspr/arosaje/client/ClientChoixArticleAccueil.java
@@ -2,19 +2,37 @@ package com.mspr.arosaje.client;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.mspr.arosaje.R;
+import com.mspr.arosaje.botanist.BotanisteChoixArticle;
+import com.mspr.arosaje.database.VolleySingleton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class ClientChoixArticleAccueil extends AppCompatActivity {
     TextView textView_nom, textView_espece, textView_date_ajout, textView_description;
     ImageView img_view;
+
+    EditText commentaire;
+
+    Button btn_ajout_commentaire;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,18 +44,63 @@ public class ClientChoixArticleAccueil extends AppCompatActivity {
         String description = intent.getStringExtra("description");
         String date = intent.getStringExtra("date");
         String url_photo = intent.getStringExtra("url_photo");
+        String id = intent.getStringExtra("id");
 
         textView_nom = (TextView) findViewById(R.id.nom_choisir);
         textView_espece = (TextView) findViewById(R.id.espece_choisir);
         textView_date_ajout = (TextView) findViewById(R.id.date_ajout_choisir);
         textView_description = (TextView) findViewById(R.id.descriptif_choisir);
         img_view = (ImageView) findViewById(R.id.click_image2);
+        btn_ajout_commentaire = (Button) findViewById(R.id.btn_ajout_commentaire_client_accueil);
+        commentaire = findViewById(R.id.champ_commentaire_client_accueil);
 
         textView_nom.setText(nom);
         textView_espece.setText(espece);
         textView_date_ajout.setText(date);
         textView_description.setText(description);
         Glide.with(this).load(url_photo).into(img_view);
+
+        btn_ajout_commentaire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v2) {
+                try {
+                    JSONObject respObj = new JSONObject();
+                    respObj.put("text", String.valueOf(commentaire.getText()));
+
+                    VolleySingleton
+                            .getInstance(ClientChoixArticleAccueil.this)
+                            .postData("/plant/" + id + "/comment", respObj, response -> Toast
+                                    .makeText(ClientChoixArticleAccueil.this, "Commentaire ajouté", Toast.LENGTH_SHORT)
+                                    .show());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        RecyclerView rvPlantsAccueil = (RecyclerView) findViewById(R.id.vertical_recycle_view_commentaire_accueil_client);
+
+        try {
+            VolleySingleton
+                    .getInstance(ClientChoixArticleAccueil.this)
+                    .getData("/plant/" + id + "/comment", response -> {
+                        try {
+                            Log.e("response", String.valueOf(response));
+                            // Initialize infoplants
+                            ArrayList<info_commentaire> infocommentaire = info_commentaire.createList(response);
+                            // Create adapter passing in the sample plant data
+                            PlantAdapter4 adapter = new PlantAdapter4(infocommentaire);
+                            // Attach the adapter to the recyclerview to populate items
+                            rvPlantsAccueil.setAdapter(adapter);
+                            // Set layout manager to position the items
+                            rvPlantsAccueil.setLayoutManager(new LinearLayoutManager(this));
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /*Bouton retour*/
